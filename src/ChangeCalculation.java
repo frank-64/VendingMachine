8ee7b092d5a316bb8437ce397ba181deb6543ec9
RnJhbmsEquals <fcminipad@gmail.com>
2020-11-13 16:06:02 +0000

    - This is the main commit of this repository as the recursiveChange() method was what I set out to try and design along with the VendingMachine and it's GUI. - CoinQuantity class introduced to store the coin value and it's corresponding quantity. - The recursiveChange() method takes the total as the starting int value V. The algorithm changes the coinQuantities ArrayList throughout each call and clears it if the change can not be made from the coins available in the ChangeCalculation constructor. - More about the algorithm and how I designed it in the README.md.

--- src/ChangeCalculation.java
+++ src/ChangeCalculation.java
@@ -1,27 +1,63 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ChangeCalculation {
     private int[] coins;
+    private ArrayList<CoinQuantity> coinQuantities;
 
     public ChangeCalculation() {
         // 1=one penny, 100=one pound...
-        coins = new int[]{2000, 1000, 500, 100, 50, 10 ,5 ,1};
+        coins = new int[]{2000,1000,100,10,1};
+        coinQuantities = new ArrayList<>();
     }
 
 
-    public static int getChange(int[] coins, int m, int V) {
+    public int recursiveChange(int count, int V) {
+        int Vmod, Vdiv;
+
+        // base case if all coins used and V did not reach 0
+        if (count > coins.length-1){
+            System.out.println(count);
+            System.out.println("Length reached!");
+            coinQuantities.clear();
+            return 0;
+        }
+        int current_coin = coins[count];
+        if(V >= current_coin){
+            Vmod = Math.floorMod(V, current_coin);
+            Vdiv = Math.floorDiv(V, current_coin);
+            if (Vmod == 0){
+                CoinQuantity coinQuantity = new CoinQuantity(current_coin, Vdiv);
+                coinQuantities.add(coinQuantity);
+                // success as the total has been divided into the coins available
+                System.out.println("Success!");
+            }else {
+                CoinQuantity coinQuantity = new CoinQuantity(current_coin, Vdiv);
+                coinQuantities.add(coinQuantity);
+                recursiveChange(count+1,Vmod);
+            }
+        }else{
+            recursiveChange(count+1, V);
+        }
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (coinQuantities.size()==0){
+            return "Change not possible with given coins!";
+        }else {
+            for (CoinQuantity cq: coinQuantities) {
+                sb.append(cq.getValue()).append(" x").append(cq.getQuantity()).append("\n");
+            }
+            return sb.toString();
+        }
     }
 
     public int[] getCoins() {
         return coins;
-    }
-
-    public static void main(String[] args) {
-        int[] coins = {15,10,5,1};
-        int V = 15;
-        System.out.println("Minimum coins required is "+ ChangeCalculation.getChange(coins, coins.length, V));
-
     }
 }
---
+++ src/CoinQuantity.java
@@ -0,0 +1,27 @@
+public class CoinQuantity {
+    private int value;
+    private int quantity;
+
+    public CoinQuantity(int value, int quantity) {
+        this.value = value;
+        this.quantity = quantity;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+}
